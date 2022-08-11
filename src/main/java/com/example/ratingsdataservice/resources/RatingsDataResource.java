5deda6327a1a432bf22486fd89a35c6f5b436356
Divyansh Dheer <divyanshdheer42@gmail.com>
2022-08-11 13:48:20 +0530

    Added cross-origin support for React FE.

--- src/main/java/com/example/ratingsdataservice/resources/RatingsDataResource.java
+++ src/main/java/com/example/ratingsdataservice/resources/RatingsDataResource.java
@@ -1,15 +1,14 @@
 package com.example.ratingsdataservice.resources;
 
 import com.example.ratingsdataservice.models.Rating;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:8085")
 @RestController
-@RequestMapping("/ratingsdata")
+@RequestMapping(value = "/ratingsdata", method = RequestMethod.GET)
 public class RatingsDataResource {
     @RequestMapping("/{movieId}")
-    public Rating getRating(@PathVariable String movieId){
+    public Rating getRating(@PathVariable String movieId) {
         return new Rating(4, movieId);
     }
 }
